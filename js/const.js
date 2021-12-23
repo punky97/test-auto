715a36c2bc3435060ac6dd5276aea1a0ee7d51a5
Hai Nguyen <hainguyen@beeketing.net>
2021-12-23 14:31:42 +0700

    extension

---
+++ js/const.js
@@ -0,0 +1,3 @@
+const ANALYTIC_URL = "https://gapi.dev.shopbase.net/admin/analytics/app?account_id="
+const ACCOUNT_URL = "https://gapi.dev.shopbase.net/admin/sales-channel/app?account_id="
+const FB_ADS = "https://www.facebook.com/adsmanager/manage"
