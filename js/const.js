164d4dd570df6e2bae75e1a56e17f2e7264157c9
Hai Nguyen <hainguyen@beeketing.net>
2022-01-10 14:04:26 +0700

    update time

--- js/const.js
+++ js/const.js
@@ -1,3 +1,2 @@
-const ANALYTIC_URL = "https://gapi.dev.shopbase.net/admin/analytics/app?account_id="
-const ACCOUNT_URL = "https://gapi.dev.shopbase.net/admin/sales-channel/app?account_id="
-const FB_ADS = "https://www.facebook.com/adsmanager/manage"
+const ANALYTIC_URL = "https://api.shopbase.com/admin/analytics/app?account_id="
+const ACCOUNT_URL = "https://api.shopbase.com/admin/sales-channel/app?account_id="
