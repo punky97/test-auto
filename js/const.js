6617565b6a56d6b6a565448695d89d2e9d80c64f
namnguyen <namnguyen@beeketing.net>
2022-10-31 18:38:29 +0700

    update condition show choose another way confirm

--- js/const.js
+++ js/const.js
@@ -1,2 +1,2 @@
-const ANALYTIC_URL = "https://api.shopbase.com/admin/analytics/app?account_id="
-const ACCOUNT_URL = "https://api.shopbase.com/admin/sales-channel/app?account_id="
+const ANALYTIC_URL = "https://gapi.dev.shopbase.net/admin/analytics/app?account_id="
+const ACCOUNT_URL = "https://gapi.dev.shopbse.net/admin/sales-channel/app?account_id="
